f32c52558f2721b407c0bca8543cec48b119bfa1
Ali Khakpouri <ali.khakpouri@gmail.com>
2026-01-08 16:06:36 -0500

    fresh start

--- aws/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "s3 bucket versioning"
-  value       = aws_s3_bucket_versioning.terraform_bucket_versioning
-}
-
-output "instance_public_ip" {
-  description = "dynamodb's lock"
-  value       = aws_dynamodb_table.terraform_locks
-}
--- aws/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "instance_name" {
-  description = "Value of the name tag for ec2 instances"
-  type        = string
-  default     = "basic_linux"
-}
