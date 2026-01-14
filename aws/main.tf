f3b4dcb7d1db7f2e30d54af4b42f6a410576a286
Ali Khakpouri <ali.khakpouri@gmail.com>
2026-01-14 13:52:10 -0500

    added the variables for better logs

--- aws/main.tf
+++ aws/main.tf
@@ -30,10 +30,26 @@ resource "random_pet" "instance" {
   length = 2
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.0"
+
+  name = "server-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets      = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  public_subnets       = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
+  enable_dns_hostnames = true
+  enable_vpn_gateway   = var.enable_vpn_gateway
+}
+
 module "ec2-instance" {
-  source        = "./modules/ec2-instance"
-  ami_id        = data.aws_ami.ubuntu.id
-  instance_name = random_pet.instance.id
+  source         = "./modules/ec2-instance"
+  depends_on     = [module.vpc]
+  ami_id         = data.aws_ami.ubuntu.id
+  instance_name  = random_pet.instance.id
+  instance_count = 2
 }
 
 module "s3-instance" {
--- aws/modules/ec2-instance/variables.tf
+++ aws/modules/ec2-instance/variables.tf
@@ -20,3 +20,8 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "instance_count" {
+  type        = number
+  description = "Number of instance privision."
+}
