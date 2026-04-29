60e382ccc7f2714c2cb775760f7376199b3341f8
Ali Khakpouri <ali.khakpouri@gmail.com>
2026-04-29 10:37:14 -0400

    added parameters

--- auth0/main.tf
+++ auth0/main.tf
@@ -1,4 +1,4 @@
-resource "auth0_branding" "commerce_api_branding" {
+resource "auth0_branding" "default_branding" {
   logo_url = "https://cdn.auth0.com/marketplace/v1/commerce-api-server.png"
   colors {
     primary         = "#0059ff"
@@ -6,11 +6,25 @@ resource "auth0_branding" "commerce_api_branding" {
   }
 }
 
-resource "auth0_prompt" "commerce_api_prompt" {
+resource "auth0_prompt" "default_prompt" {
   universal_login_experience = "new"
   identifier_first           = true
 }
 
 module "commerce_api" {
   source = "./modules/api"
+
+  name       = "Commerce Api Server"
+  identifier = "commerce-api-server"
+  scopes = {
+    "orders:read"     = "read orders"
+    "orders:write"    = "write orders"
+    "orders:delete"   = "delete orders"
+    "products:read"   = "read products"
+    "products:write"  = "write products"
+    "products:delete" = "delete products"
+    "users:read"      = "read users"
+    "users:write"     = "write users"
+    "users:delete"    = "delete users"
+  }
 }
--- auth0/modules/api/outputs.tf
+++ auth0/modules/api/outputs.tf
@@ -1,14 +1,14 @@
 output "audience" {
   description = "Audience identifier for the resource server. Consumers use this as the JWT 'aud' claim and as the `audience` parameter when requesting access tokens."
-  value       = auth0_resource_server.commerce_api.identifier
+  value       = auth0_resource_server.respource_server.identifier
 }
 
 output "name" {
   description = "Display name of the resource server (as shown in the Auth0 dashboard)."
-  value       = auth0_resource_server.commerce_api.name
+  value       = auth0_resource_server.respource_server.name
 }
 
 output "scope_names" {
   description = "List of scope names registered on the resource server."
-  value       = [for s in auth0_resource_server_scopes.commerce_api_scope.scopes : s.name]
+  value       = [for s in auth0_resource_server_scopes.resource_server_scopes.scopes : s.name]
 }
